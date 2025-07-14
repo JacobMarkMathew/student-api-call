c802577778c83233506c4ad2acb5305ca5c7b682
jacob markmathew <jacobmathew00455@gmail.com>
2025-07-14 19:14:46 +0530

    api

--- src/components/Deletestudent.jsx
+++ src/components/Deletestudent.jsx
@@ -9,7 +9,7 @@ const Deletestudent = () => {
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <div className="row">
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                             <label htmlFor="" className="form-label">Roll no</label>
                             <input type="number" className="form-control" />
